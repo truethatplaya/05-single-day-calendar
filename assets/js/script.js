f5e3542d3ddf439d6baba57b652aa452f38fa47a
truethatplaya <alina.hernandez.ca@gmail.com>
2021-04-26 02:36:00 -0700

    rows are in place. time is working and adding seconds on load. local storage is saving.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,33 @@
-//Current Date
-var today = moment().format("dddd, MMMM Do, YYYY");
-$("#today").text(today);
+//global variables
+var timeDisplayEl = $("#rightNow");
+
+//Current Date and Time
+function displayTime() {
+  var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+  timeDisplayEl.text(rightNow);
+}
+
+//Save Appointment
+function saveAppointment() {
+  var storeAppointment = localStorage.getItem("textArea");
+  console.log(storeAppointment);
+}
+
+var saveBtn = document.querySelector(".saveBtn");
+var userInput = document.querySelector("textArea");
+
+//when save button is clicked
+saveBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  saveAppointment();
+
+  // create object from user input
+  var appointmentInfo = {
+    appointment: userInput.value.trim(),
+  };
+
+  // set user input to local storage
+  localStorage.setItem("appointmentInfo", JSON.stringify(appointmentInfo));
+});
+
+setInterval(displayTime, 1000);
