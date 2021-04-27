9cd26418aa08536a1091fff8726ee00804ea8bab
truethatplaya <alina.hernandez.ca@gmail.com>
2021-04-26 17:46:39 -0700

    started to refractor using jquery instead of .html. time is still working, commented out local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,81 @@
-//global variables
 var timeDisplayEl = $("#rightNow");
+var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
+var currentHour = moment().format("hh");
 
-//Current Date and Time
+//----------Current Date and Time --------
 function displayTime() {
   var rightNow = moment().format("MMM DD, YYYY [at] hh:mm:ss a");
   timeDisplayEl.text(rightNow);
 }
 
-//Save Appointment
-function saveAppointment() {
-  var storeAppointment = localStorage.getItem("textArea");
-  console.log(storeAppointment);
-}
+// var timeSlotBackgroud = $("#textArea");
+// var hours = [7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7];
+// var militaryTime = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+// var hourName = [
+//   "hour9",
+//   "hour10",
+//   "hour11",
+//   "hour12",
+//   "hour1",
+//   "hour2",
+//   "hour3",
+//   "hour4",
+//   "hour5",
+//   "hour6",
+//   "hour7",
+// ];
+// var saveBtn = $(".saveBtn");
+// var userInput = $("#appointmentTitle");
 
-var saveBtn = document.querySelector(".saveBtn");
-var userInput = document.querySelector("textArea");
+// for (var i = 0; i < hours.length; i++) {
+//   var newRow = $("<row>");
+//   $(".container").append(newRow);
 
-//when save button is clicked
-saveBtn.addEventListener("click", function (event) {
-  event.preventDefault();
-  saveAppointment();
+//   var newTimeCol = $("<col>").addClass("hour", "col-3 hour").text(hours[i]);
+//   var newInputCol = $("<col>")
+//     .addClass("input", "col-8 input")
+//     .html("<textArea rows='2' style='width: 100%; height: 100%'></textArea>");
+//   newRow.append(newTimeCol, newInputCol);
+//   newInputCol.attr("id", hourName[i]);
 
-  // create object from user input
-  var appointmentInfo = {
-    appointment: userInput.value.trim(),
-  };
+//   newRow.addClass("timeBlock row");
+//   newRow.attr("id", militaryTime[i]);
 
-  // set user input to local storage
-  localStorage.setItem("appointmentInfo", JSON.stringify(appointmentInfo));
-});
+//   var newBtn = $("<button>").addClass("saveBtn", "fas fa-save col-1");
+//   newRow.append(newBtn);
+
+//   if (hours[i] === 12) {
+//     newTimeCol.text(hours[i] + "PM");
+//   } else if (hours[i] < 12) {
+//     newTimeCol.text(hours[i] + "AM");
+//   } else {
+//     newTimeCol.text(hours[i] + "PM");
+//   }
+// }
+// //todo on click
+// // var userInputLocation = document.getElementById("#locationInput");
+// var lsOutput = $("#lSOutput");
+// // //Save Appointment to local storage
+// // saveBtn.addEventListener("click", function () {
+// //   getUserInputFromLocalStorage();
+// // });
+
+// // //   // create object from user input
+// // //   var appointmentInfo = {
+// // //     appointment: userInput.value.trim(),
+// // //   };
+
+// // //   // set user input to local storage
+// // // //   window.localStorage.setItem(
+// // // //     "title, descrption",
+// // // //     JSON.stringify(appointmentInfo)
+// // // // //   );
+// // // // // });
+
+// // // function getUserInputFromLocalStorage() {
+// // //   window.localStorage.getItem("appointmentInput);
+// // //   JSON.parse(window.localStorage.getItem("appointmentInfo"));
+// // //   console.log(localStorage);
+// // // }
 
 setInterval(displayTime, 1000);
